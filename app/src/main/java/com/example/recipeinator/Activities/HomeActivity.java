cdf43be4d8aa01e0c7442badb558422280671933
Adin Halilovic <adin.halilovic15@gmail.com>
2021-06-21 17:02:01 +0200

    Added username

--- app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
+++ app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.recipeinator.Activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.recipeinator.BottomNavbarListener;
 import com.example.recipeinator.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -15,5 +20,7 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView bottomNavbar = findViewById(R.id.bottom_navbar);
         bottomNavbar.setSelectedItemId(R.id.page_home);
         bottomNavbar.setOnNavigationItemSelectedListener(new BottomNavbarListener(this));
+
+        username = findViewById(R.id.home_usernameTxt);
     }
 }
