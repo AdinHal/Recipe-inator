1fe8796d1c7456f91efcbea6202a88ae0e93c1e4
Adin Halilovic <adin.halilovic15@gmail.com>
2021-06-21 17:00:05 +0200

    Code commented

--- app/src/main/java/com/example/recipeinator/neu/RegisterActivity.java
+++ app/src/main/java/com/example/recipeinator/neu/RegisterActivity.java
@@ -13,32 +13,37 @@ import com.example.recipeinator.models.User;
 
 public class RegisterActivity extends AppCompatActivity {
     private DatabaseAdapter databaseAdapter;
-
     private EditText nickname, name, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        databaseAdapter = DatabaseAdapter.getInstance(this);
 
+        // Assigning values
+        databaseAdapter = DatabaseAdapter.getInstance(this);
         nickname = findViewById(R.id.register_nickname);
         name = findViewById(R.id.register_name);
         email = findViewById(R.id.register_email);
         password = findViewById(R.id.register_password);
     }
     public void createAccount(View view){
+        // Creating separate strings for cleaner code
         String nick = nickname.getText().toString();
         String fullname = name.getText().toString();
         String mail = email.getText().toString();
         String pw = password.getText().toString();
 
+        // Calling registerUser and creating a new User with the strings above
         registerUser(new User(nick,fullname,mail,pw));
     }
     public void registerUser(User user){
+        // Adding the new user to the database
         databaseAdapter.addNewUser(user);
         startActivity(new Intent(this, HomeActivity.class));
     }
+
+    // Method on TextView in activity_register.xml ("Already a member? Log in")
     public void alreadyMember(View view){
         startActivity(new Intent(this, LoginActivity.class));
     }
