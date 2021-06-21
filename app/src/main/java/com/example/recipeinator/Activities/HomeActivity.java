a7c5d1e88f2cf0755faaef1380ad1292e56da6eb
Martin Unterhuber <m.unterhuber99@gmail.com>
2021-06-21 12:44:38 +0200

    Added Detail Activity for Recipe

--- app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
+++ app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.recipeinator.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.recipeinator.BottomNavbarListener;
+import com.example.recipeinator.MainActivity;
 import com.example.recipeinator.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
