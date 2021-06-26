d2bec7bad36b4214c032ae71f0029bdb9ddbd4cf
Adin Halilovic <adin.halilovic15@gmail.com>
2021-06-26 10:32:47 +0200

    Added Hint in Serachbar

--- app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
+++ app/src/main/java/com/example/recipeinator/Activities/HomeActivity.java
@@ -1,15 +1,12 @@
 package com.example.recipeinator.Activities;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.Toast;
 
